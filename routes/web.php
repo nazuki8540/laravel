a13b428f61adc90a0c73efedf692775dd871332c
root <root@DESKTOP-0FC6T5J.localdomain>
2023-08-14 18:04:15 -0300

    adicionando Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\SiteController;
+use App\Http\Controllers\Admin\SupportController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', [SiteController::class, 'contact']);
+
+Route::get('/supports',[SupportController::class, 'index'])->name('supports.index');
